cf1c4604729e0f22e4784a4071a4c73dab8e37f1
zander <zander.riggs@gmail.com>
2021-08-29 18:59:38 -0400

    Style changes only - Update to ESLint file - Linting of index.js file for format updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,71 +1,68 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import "../styles/style.scss"
+import * as React from 'react';
+import { Link } from 'gatsby';
+import '../styles/style.scss';
 
 // data
 const links = [
-  {
-    text: "How To Grapple",
-    url: "/grapple",
-    badge: false,
-    description:
-      "Grappling is fun and cool. It's fairly straight-forward, but sometimes we just have to make sure we remember how it works.",
-    color: "#E95800",
-  },
-  {
-    text: "Conditions",
-    url: "/conditions",
-    badge: true,
-    description:
-      "There's a lot of conditions that a character can be subject to. Find them here.",
-    color: "#1099A8",
-  },
-  {
-    text: "Measuring Skill DCs",
-    url: "",
-    badge: false,
-    description:
-      "We all think we have an idea of what's difficult. But what's a challege?",
-    color: "#1099A8",
-  }
-]
+    {
+        text: 'How To Grapple',
+        url: '/grapple',
+        badge: false,
+        description:
+            "Grappling is fun and cool. It's fairly straight-forward, but sometimes we just have to make sure we remember how it works.",
+        color: '#E95800',
+    },
+    {
+        text: 'Conditions',
+        url: '/conditions',
+        badge: true,
+        description: "There's a lot of conditions that a character can be subject to. Find them here.",
+        color: '#1099A8',
+    },
+    {
+        text: 'Measuring Skill DCs',
+        url: '',
+        badge: false,
+        description: "We all think we have an idea of what's difficult. But what's a challege?",
+        color: '#1099A8',
+    },
+];
+
 
 // markup
 const IndexPage = () => {
-  return (
-    <main class="page-styles">
-      <title>Home Page</title> {/* how/why does this work? lol */}
-      <h1>
-        How To TTRPG
-      </h1>
-      <h2 className="reference-title">— A Reference Guide to <br /> ~Doing Things~ </h2>
-      <h3>
-        <Link to="/about">About</Link>
-      </h3>
-      <p>
-        I just adjusted the auto-generated index.js file to be a super simple example of what the main page could look like. 
-        idk if we want to have all the info here or link to other pages/domains. 
-        But I figured we'd just start the thing.
-      </p>
-      <ul>
-        {links.map(link => (
-          <li key={link.url} style={{ color: link.color }}>
-            <span>
-              <a href={link.url}>
-                {link.text}
-              </a>
-              {link.badge && (
-                <span className="new-badge" aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p class="description">{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-    </main>
-  )
-}
+    return (
+        <main className="page-styles">
+            <title>Home Page</title> {/* how/why does this work? lol */}
+            <h1>How To TTRPG</h1>
+            <h2 className="reference-title">
+                — A Reference Guide to <br /> ~Doing Things~{' '}
+            </h2>
+            <h3>
+                <Link to="/about">About</Link>
+            </h3>
+            <p>
+                I just adjusted the auto-generated index.js file to be a super simple example of what the main page
+                could look like. idk if we want to have all the info here or link to other pages/domains. But I figured
+                we'd just start the thing.
+            </p>
+            <ul>
+                {links.map(link => (
+                    <li key={link.url} style={{ color: link.color }}>
+                        <span>
+                            <a href={link.url}>{link.text}</a>
+                            {link.badge && (
+                                <span className="new-badge" aria-label="New Badge">
+                                    NEW!
+                                </span>
+                            )}
+                            <p className="description">{link.description}</p>
+                        </span>
+                    </li>
+                ))}
+            </ul>
+        </main>
+    );
+};
 
-export default IndexPage
+export default IndexPage;
