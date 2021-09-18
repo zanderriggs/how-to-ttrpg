620a44fef0fa7e8e58b0b7808521951f935a36fa
mike ¯\_(ツ)_/¯ <mikerapin@gmail.com>
2021-09-18 14:10:24 -0400

    Added simple Die component for visual die
    For now this only shows d4 and d8 as those were easiest to implement.

---
+++ src/components/die.tsx
@@ -0,0 +1,54 @@
+import '../styles/die.scss';
+
+import * as React from 'react';
+
+import { DieType } from '../types/dc-types';
+
+export const Die = ({
+    die,
+    inline = true,
+    size = 16,
+}: {
+    die: DieType;
+    size?: number;
+    inline?: boolean;
+}): React.ReactElement => {
+    const containerClass = `die-container d${die.sides} ${inline ? 'inline-die' : ''}`;
+
+    const getDie = (): React.ReactElement => {
+        switch (die.sides) {
+            case 2:
+            case 6:
+            case 10:
+            case 12:
+            case 20:
+            case 100:
+                return <>die</>;
+            case 4:
+                return (
+                    <>
+                        <div className="eqt"></div>
+                        <div className="eqt-left"></div>
+                    </>
+                );
+            case 8:
+                return (
+                    <>
+                        <div className="eqt"></div>
+                        <div className="eqt-left"></div>
+                        <div className="eqt-right"></div>
+                        <div className="eqt-bottom"></div>
+                    </>
+                );
+        }
+    };
+
+    const dieSizeCssVariable = { '--die-size': `${size}px` } as React.CSSProperties;
+
+    return (
+        <div style={dieSizeCssVariable} className={containerClass}>
+            <div className={`die-face ${die.sides}-face`}>{die.sides}</div>
+            {getDie()}
+        </div>
+    );
+};
