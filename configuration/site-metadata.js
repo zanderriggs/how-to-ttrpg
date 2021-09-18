0a920805c214957025d98f8d871dd71347bd5afe
mike ¯\_(ツ)_/¯ <mikerapin@gmail.com>
2021-09-18 02:42:03 -0400

    Adds basic menu with dynamic pages from the config
    - Added new menu with some real simple CSS animation. The styling is
      _not very good_ but it's a start!
    - New pages can be added via the configuration/site-metadata.js file
    - Added two new icons as JSX from Font-Awesome 5 (I have a commercial license)

---
+++ configuration/site-metadata.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+module.exports = siteMetadata = {
+    siteUrl: 'https://tbd.tld',
+    title: 'How To TTRPG',
+    menuLinks: [
+        {
+            name: 'Home',
+            link: '/',
+        },
+        {
+            name: 'About',
+            link: '/about',
+        },
+        {
+            name: 'Difficulty Checks',
+            link: '/difficulty',
+        },
+        {
+            name: 'Grappling',
+            link: '/grapple',
+        },
+    ],
+};
